1d791a40d35f094e9cde77064de765b0035ba684
esote <esote.net@gmail.com>
2019-08-25 19:28:04 -0500

    Used within a reverse proxy

--- fmtc.go
+++ fmtc.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
-	"flag"
 	"log"
 	"net"
 	"net/http"
@@ -52,8 +50,12 @@ const indexHTML = `<!DOCTYPE html>
 
 func setHeaders(w http.ResponseWriter) {
 	w.Header().Set("Referrer-Policy", "no-referrer")
+
+	// Used within a reverse proxy, Strict-Transport-Security header is
+	// copied to the proxy.
 	w.Header().Set("Strict-Transport-Security", "max-age=31536000;"+
 		"includeSubDomains;preload")
+
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("X-Frame-Options", "deny")
 	w.Header().Set("X-XSS-Protection", "1")
@@ -127,12 +129,6 @@ func main() {
 		l.Close()
 	}
 
-	// letsencrypt key and cert, /etc/letsencrypt/live symlinks to archive/
-
-	if err := unix.Unveil("/etc/letsencrypt/archive/", "r"); err != nil {
-		log.Fatal(err)
-	}
-
 	if err := unix.Unveil("./indent.out", "x"); err != nil {
 		log.Fatal(err)
 	}
@@ -146,38 +142,13 @@ func main() {
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/format", format)
 
-	cfg := &tls.Config{
-		MinVersion: tls.VersionTLS12,
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP521,
-			tls.X25519,
-		},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
-
 	srv := &http.Server{
-		Addr:         ":8443",
-		Handler:      mux,
-		TLSConfig:    cfg,
-		TLSNextProto: nil,
+		Addr:    ":8443",
+		Handler: mux,
 	}
-
-	var (
-		cert string
-		key  string
-	)
-
-	flag.StringVar(&cert, "cert", "server.crt", "TLS certificate file")
-	flag.StringVar(&key, "key", "server.key", "TLS key file")
-
-	flag.Parse()
 
 	graceful.Graceful(srv, func() {
-		if err := srv.ListenAndServeTLS(cert, key); err != http.ErrServerClosed {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 			log.Fatal(err)
 		}
 	}, os.Interrupt)
